e07db955de0163f7b35fecf0fc8a8742544fdc45
Pheng Heong TAN <phtan90@gmail.com>
2016-09-07 16:26:34 +0800

    Fix an error with parsing some XML file.
    Empty co-ordinates should not cause an error but they did. This commit fixes that.

--- editor/xmlToShapefile.py
+++ editor/xmlToShapefile.py
@@ -16,44 +16,44 @@ class XmlToShapefile(QObject):
         self.formula = formula
 
     def parseLocation(self, data):
-        x = float(data.find("xPos").text)
-        y = float(data.find("yPos").text)
+ 
+        searchX = data.find("x").text
+        searchY = data.find("y").text
+        x = float(searchX) 
+        y = float(searchY) 
         
         pos = QgsPoint(eval(self.formula[0]), eval(self.formula[1]))
-        data.find("xPos").text = "--"
-        data.find("yPos").text = "--"
+        # data.find("x").text = "--"                                    Won't parse out location
+        # data.find("y").text = "--"
         return pos
 
-    def parseUninode(self, uninode):
-        nodeId = uninode.find("nodeID").text
-        location = uninode.find("location")
-        if location is None:
-            QgsMessageLog.logMessage("No location in uninode %s"%str(nodeId), 'SimGDC')
-            return
-        point = self.parseLocation(location)
-        attr = [nodeId]
-        self.writer.addPoint(SHTYPE.UNINODE, point, attr)
-
     def parseMulnode(self, mulnode):
-        nodeId = mulnode.find("nodeID").text
-        location = mulnode.find("location")
-        if location is None:
-            QgsMessageLog.logMessage("No location in multinode %s"%str(nodeId), 'SimGDC')
+        nodeId = mulnode.find("id").text
+        point = mulnode.find("point")
+        if point is None:
+            QgsMessageLog.logMessage("No polypoint in node %s"%str(nodeId), 'SimGDC')
             return        
-        point = self.parseLocation(location)
+        point = self.parseLocation(point)
         attr = [nodeId]
-        self.writer.addPoint(SHTYPE.MULNODE, point, attr)
+        turningPaths = mulnode.find("turning_path")
+
+        self.writer.addPoint(SHTYPE.NODE, point, attr)
 
     def parseLane(self, segmentId, lane):
-        landId = lane.find("laneID").text
-        attr = [segmentId, landId]
+        laneId = lane.find("id").text
+        attr = [segmentId, laneId]
         coordinates = []
-        polyLine = lane.find("PolyLine")
+        polyLine = lane.find("polyline")
         if polyLine is None:
-            QgsMessageLog.logMessage("No PolyLine in lane %s"%str(landId), 'SimGDC')
-            return  
-        for polypoint in polyLine.findall('PolyPoint'):
-            coordinates.append(self.parseLocation(polypoint.find('location')))
+            QgsMessageLog.logMessage("No polyline in lane %s"%str(laneId), 'SimGDC')
+            return
+        points = polyLine.find("points")
+        for point in points.findall("point"):
+            coordinates.append(self.parseLocation(point))
+        if len(coordinates) == 0:
+            QgsMessageLog.logMessage("Lane %s has no polyline info."%laneId, 'SimGDC')
+            return
+
         self.writer.addPolyline(SHTYPE.LANE, coordinates, attr)
 
     def parseLaneEdge(self, segmentId, laneEdge):
@@ -64,7 +64,7 @@ class XmlToShapefile(QObject):
         if polyLine is None:
             QgsMessageLog.logMessage("No polyline in laneEdge %s of segment %s"%(str(laneNumber),str(segmentId)), 'SimGDC')
             return            
-        for polypoint in polyLine.findall('PolyPoint'):
+        for polypoint in polyLine.findall('point'):
             coordinates.append(self.parseLocation(polypoint.find('location')))
         self.writer.addPolyline(SHTYPE.LANEEDGE, coordinates, attr)
     
@@ -75,7 +75,7 @@ class XmlToShapefile(QObject):
         nearLine = crossing.find("nearLine")
         if nearLine is None:
             QgsMessageLog.logMessage("No nearLine in crossing %s"%crossingId, 'SimGDC')
-            return 
+            return
         coordinates[0] = self.parseLocation(nearLine.find('first'))
         coordinates[1] = self.parseLocation(nearLine.find('second'))
         farLine = crossing.find("farLine")
@@ -86,86 +86,142 @@ class XmlToShapefile(QObject):
         coordinates[2] = self.parseLocation(farLine.find('second'))
         self.writer.addPolygon(SHTYPE.CROSSING, coordinates, attr)
 
-    def parseBusstop(self, segmentId, busstop):
-        point = self.parseLocation(busstop)
-        attr = [segmentId, busstop.find("id").text]
-        self.writer.addPoint(SHTYPE.BUSSTOP, point, attr) 
+    def parseTurningPath(self, turningpath):
+
+        id = turningpath.find("id").text
+        groupID = turningpath.find("group_id").text
+        attr = [id, groupID]
+        coordinates = []
+        polyline = turningpath.find("polyline")
+        if polyline is None:
+            QgsMessageLog.logMessage("Turning Path %s has no polyline info."%id, 'SimGDC')
+            return
+        points = polyline.find("points")
+        for point in points.findall("point"):
+            x = point.find("x")
+            xtext = x.text
+            if xtext is None:
+                QgsMessageLog.logMessage("Point in turning path %s has no co-ordinate info"%id, "SimGDC")
+                continue
+            coordinates.append(self.parseLocation(point))
+        if len(coordinates) == 0:
+            QgsMessageLog.logMessage("Turning Path %s has no polyline info."%id, 'SimGDC')
+            return
+
+        self.writer.addPolyline(SHTYPE.TURNINGPATH,coordinates, attr)
+
+    def parseLink(self, link):
+
+        id = link.find("id").text
+        road_name = link.find("road_name").text
+        attr = [id,road_name]
+        coordinates = []
+        polyline = link.find("polyline")
+        if polyline is None:
+            QgsMessageLog.logMessage("Link %s has no polyline info."%id, 'SimGDC')
+            return
+        points = polyline.find("points")
+        for point in points.findall("point"):
+            coordinates.append(self.parseLocation(point))
+        if len(coordinates) == 0:
+            QgsMessageLog.logMessage("Link %s has no polyline info."%id, 'SimGDC')
+            return
+
+        self.writer.addPolyline(SHTYPE.LINK,coordinates, attr)
+
+
+    def parseBusstop(self, busstop):
+        point = busstop.find("point")
+        x = point.find("x")
+        text = x.text
+        if text is None:
+            QgsMessageLog.logMessage("Point in busstop has no co-ordinate info", "SimGDC")
+            return
+        coordinates = self.parseLocation(point)
+        attr = [busstop.find("segment_id").text, busstop.find("id").text]
+        self.writer.addPoint(SHTYPE.BUSSTOP, coordinates, attr)
+
+    def parseTrainstop(self, trainstop):
+        point = trainstop.find("point")
+        x = point.find("x")
+        text = x.text
+        if text is None:
+            QgsMessageLog.logMessage("Point in trainstop has no co-ordinate info", "SimGDC")
+            return
+        coordinates = self.parseLocation(point)
+        attr = ["".join(trainstop.findall("segment_id")), trainstop.find("id").text]
+        self.writer.addPoint(SHTYPE.TRAINSTOP, coordinates, attr)
 
     def parseSegment(self, linkId, segment):
-        segmentId = segment.find("segmentID").text
-        speedLimit = segment.find("maxSpeed").text
-        attr = [linkId, segmentId, speedLimit]
+        segmentId = segment.find("id").text
+        attr = [linkId, segmentId]
         coordinates = [] 
         polyline = segment.find("polyline")
         if polyline is None:
             QgsMessageLog.logMessage("segment %s has no polyline info."%segmentId, 'SimGDC')
             return
-        for polypoint in polyline.findall('PolyPoint'):
-            coordinates.append(self.parseLocation(polypoint.find('location')))
+        points = polyline.find("points")
+        for point in points.findall("point"):
+            coordinates.append(self.parseLocation(point))
         if len(coordinates) == 0:
-            QgsMessageLog.logMessage("segment %s has no polygon info."%segmentId, 'SimGDC')
+            QgsMessageLog.logMessage("segment %s has no polyline info."%segmentId, 'SimGDC')
             return
-        if len(coordinates) < 3:
-            coordinates.append(QgsPoint(coordinates[0]))
-            coordinates.append(QgsPoint(coordinates[1]))
+        # if len(coordinates) < 3:
+        #     coordinates.append(QgsPoint(coordinates[0]))
+        #     coordinates.append(QgsPoint(coordinates[1]))
         #parse Lane
-        lanes = segment.find("Lanes")
+        lanes = segment.find("lanes")
         if lanes is not None:
-            for lane in lanes.findall('Lane'):
+            for lane in lanes.findall('lane'):
                 self.parseLane(segmentId, lane)
-        #parse Lane Egde
-        laneEdges = segment.find("laneEdgePolylines_cached")
-        if laneEdges is not None:
-            for laneEdge in laneEdges.findall('laneEdgePolyline_cached'):
-                self.parseLaneEdge(segmentId, laneEdge)
-        #parse obstacles
-        obstacles = segment.find("Obstacles")
-        if obstacles is not None:
-            for obstacle in obstacles.iter():
-                if obstacle.tag == "Crossing":
-                    self.parseCrossing(segmentId, obstacle)
-                elif obstacle.tag == "BusStop":
-                    self.parseBusstop(segmentId, obstacle)
-        self.writer.addPolygon(SHTYPE.SEGMENT, coordinates, attr)
+        # #parse Lane Edge
+        # laneEdges = segment.find("laneEdgePolylines_cached")
+        # if laneEdges is not None:
+        #     for laneEdge in laneEdges.findall('laneEdgePolyline_cached'):
+        #         self.parseLaneEdge(segmentId, laneEdge)
+
+        self.writer.addPolyline(SHTYPE.SEGMENT, coordinates, attr)
 
     def run(self):
         if self.document == None:
             return
         progPercent = 0
-        roadNetwork = self.document.find('GeoSpatial/RoadNetwork')
+        roadNetwork = self.document.find('road_network')
         #parse nodes
-        nodes = roadNetwork.find('Nodes')
+        nodes = roadNetwork.find('nodes')
         if nodes is not None:
-            uniNodes = []
-            mulNodes = []
-            uniNodeParent = nodes.find('UniNodes')
-            if uniNodeParent is not None:
-                uniNodes = uniNodeParent.findall('UniNode')
-            mulNodeParent = nodes.find('Intersections')
-            if mulNodeParent is not None:
-                mulNodes = mulNodeParent.findall('Intersection')
+            mulNodes = nodes.findall('node')
 
-            count = len(uniNodes) + len(mulNodes)
-            for uninode in uniNodes:
-                self.parseUninode(uninode)
-                progPercent = progPercent + 50.0/count
-                self.prog_sig.emit(progPercent)
+            count = len(mulNodes)
             for mulNode in mulNodes:
                 self.parseMulnode(mulNode)
                 progPercent = progPercent + 50.0/count
                 self.prog_sig.emit(progPercent)
 
-        #parse segment
+
+        for turningpath in roadNetwork.iter('turning_path'):
+            self.parseTurningPath(turningpath)
+
+        #parse obstacles
+        pt_stops = roadNetwork.find("pt_stops")
+        if pt_stops is not None:
+            for bus_stop in pt_stops.iter('bus_stop'):
+                self.parseBusstop(bus_stop)
+            for train_stop in pt_stops.iter('train_stop'):
+                self.parseTrainstop(train_stop)
+
+        #parse link,segment
         links = []
-        linkParent = roadNetwork.find('Links')
+        linkParent = roadNetwork.find('links')
         if linkParent is not None:
-            links = linkParent.findall('Link')
+            links = linkParent.findall('link')
         count = len(links)
         for link in links:
-            linkId = link.find('linkID').text
-            segmentParent = link.find('Segments')
+            self.parseLink(link)
+            linkId = link.find('id').text
+            segmentParent = link.find('segments')
             if segmentParent is not None:
-                segments = segmentParent.findall('Segment')
+                segments = segmentParent.findall('segment')
                 if segments is not None:
                     for segment in segments:
                         self.parseSegment(linkId, segment)
